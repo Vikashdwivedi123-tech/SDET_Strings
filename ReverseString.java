6534482b047f15c54a43e140e80a8db617c4ddcd
Vikash <dwivedi@Mac.lan>
2026-05-23 16:50:00 +0530

    update code of revers string

--- ReverseString.java
+++ ReverseString.java
@@ -2,14 +2,12 @@ package SDET_Strings;
 
 public class ReverseString {
     public static void main(String[] args) {
-//        String str = "Vikash";
-        System.out.println(reversse("Vikash"));
-    }
-    static String reversse(String str){
-        StringBuilder res = new StringBuilder();
-        for(int i= str.length()-1;i>=0;i--){
-            res.append(str.charAt(i));
+        String name = "Vikash";
+        String rev = "";
+        for(int i=name.length()-1;i>=0;i--){
+            rev+=name.charAt(i);
         }
-        return res.toString();
+        System.out.println("revers string is: "+rev);
     }
+
 }
