0cb905131d9fc5504ac43267a3bb08b8bee26f22
Vikash <dwivedi@Mac.lan>
2026-05-24 14:28:59 +0530

    update reverse sentence method

--- ReverseWordsOrderInSentence.java
+++ ReverseWordsOrderInSentence.java
@@ -2,12 +2,12 @@ package SDET_Strings;
 
 public class ReverseWordsOrderInSentence {
     public static void main(String[] args) {
-        System.out.println(revWords("apple banana kiwi")); // kiwi banana apple
+        System.out.println(revWords("Welcome to java")); // kiwi banana apple
         System.out.println(revWords("I am John Doe")); // Doe John am I
         System.out.println(revWords("orange")); // orange
     }
     public static String revWords(String str) {
-        StringBuilder res = new StringBuilder();
+        String res = "";
 
         // split input string by " " space to get each word as String[]
         String[] words = str.split(" ");
@@ -15,10 +15,10 @@ public class ReverseWordsOrderInSentence {
         // loop over the array from back
         for(int i = words.length - 1; i >= 0; i--) {
             // add words to res with space
-            res.append(words[i]).append(" "); // just reversing the word and adding space in the end!
+            res = res+words[i]+" ";// just reversing the word and adding space in the end!
         }
 
         // trim needed to remove last space in the end
-        return res.toString().trim();
+        return res;
     }
 }
